080a8c46bf66fc95e2698c870863bcd3621d3b4c
ejxzhn22 <ejxzhn22@naver.com>
2021-07-01 19:15:54 +0900

    .

---
+++ src/main/java/com/spring/itjunior/ItjuniorApplication.java
@@ -0,0 +1,13 @@
+package com.spring.itjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItjuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItjuniorApplication.class, args);
+	}
+
+}
