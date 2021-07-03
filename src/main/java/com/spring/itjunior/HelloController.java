fb400f41017cefb4b43f375c3398c11a6c6aee36
ufonetcom <ufozx@naver.com>
2021-07-03 17:29:31 +0900

    branch test.. 변경사항을 jaebok 브랜치에서 나의 원격 저장소로 (origin) commit and push 한다.

--- src/main/java/com/spring/itjunior/HelloController.java
+++ src/main/java/com/spring/itjunior/HelloController.java
@@ -4,15 +4,16 @@ package com.spring.itjunior;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    @Controller
-    @RequestMapping("/hello")
-    public class HelloController {
+@RestController
+@RequestMapping
+public class HelloController {
 
-        @GetMapping
-        public String hello() {
-            return "hello";
-        }
+    @GetMapping("/test")
+    public String hello() {
+        return "hello api 출력";
     }
+}
 
 
