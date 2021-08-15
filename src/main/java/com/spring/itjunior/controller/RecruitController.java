da73c4da22ce3f11a9b8a00566659e651ab576c0
ufonetcom <ufozx@naver.com>
2021-08-15 19:30:53 +0900

    아이디저장 구현 완료,, 마이페이지 스크랩확인 기능 구현중,,

--- src/main/java/com/spring/itjunior/controller/RecruitController.java
+++ src/main/java/com/spring/itjunior/controller/RecruitController.java
@@ -52,7 +52,7 @@ public class RecruitController {
 //        model.addAttribute("categories", categories);
 //        model.addAttribute("result",resultInfo);
 //        model.addAttribute("page",setPageDto);
-
+//        git test token
         return "job/jobList";
     }
 
